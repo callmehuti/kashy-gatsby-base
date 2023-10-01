ae159c23f4a83232cff82a380e2ba5de3f23a1ff
callmehuti <htinvobnesgn@gmail.com>
2023-10-01 15:57:03 +1000

    succesful display media img

--- src/components/content/home-page/media.tsx
+++ src/components/content/home-page/media.tsx
@@ -7,11 +7,28 @@ import HPMediaPhotoItem from "../../common/media-img";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const HPMedia: FC = () => {
+// Import image sources
+// Import image sources
+import thumb1 from "../../../assets/img/thumb-1.jpeg";
+import thumb2 from "../../../assets/img/thumb-2.jpeg";
+import thumb3 from "../../../assets/img/thumb-3.jpeg";
+import thumb4 from "../../../assets/img/thumb-4.jpeg";
+import thumb5 from "../../../assets/img/thumb-5.jpeg";
+import thumb6 from "../../../assets/img/thumb-6.jpeg";
+import thumb7 from "../../../assets/img/thumb-7.jpeg";
+import thumb8 from "../../../assets/img/thumb-8.jpeg";
 
+const HPMedia: FC = () => {
   const imageSrcs = [
-    require("../../../assets/img/customer-1.jpeg")
-  ]
+    thumb1,
+    thumb2,
+    thumb3,
+    thumb4,
+    thumb5,
+    thumb6,
+    thumb7,
+    thumb8,
+  ];
 
   return (
     <div className="media-inner">
@@ -43,7 +60,14 @@ const HPMedia: FC = () => {
         </div>
 
         <div className="media-photo__list">
-          <HPMediaPhotoItem imageSrcs={imageSrcs}/>
+          <HPMediaPhotoItem imageSrcs={[thumb1]} />
+          <HPMediaPhotoItem imageSrcs={[thumb2]} />
+          <HPMediaPhotoItem imageSrcs={[thumb3]} />
+          <HPMediaPhotoItem imageSrcs={[thumb4]} />
+          <HPMediaPhotoItem imageSrcs={[thumb5]} />
+          <HPMediaPhotoItem imageSrcs={[thumb6]} />
+          <HPMediaPhotoItem imageSrcs={[thumb7]} />
+          <HPMediaPhotoItem imageSrcs={[thumb8]} />
         </div>
       </div>
     </div>
